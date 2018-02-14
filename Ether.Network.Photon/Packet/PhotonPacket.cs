2d147aa6d484e613cdfd219b99a356a2c721e2e0
johmarjac <johannesmjacobs@gmail.com>
2018-02-14 17:02:27 +0100

    Some PhotonPacket Additions

--- Ether.Network.Photon/Packet/PhotonPacket.cs
+++ Ether.Network.Photon/Packet/PhotonPacket.cs
@@ -1,4 +1,6 @@
-﻿namespace Ether.Network.Photon.Packet
+﻿using System;
+
+namespace Ether.Network.Photon.Packet
 {
     public class PhotonPacket : PhotonPacketStream
     {
@@ -13,5 +15,49 @@
         }
 
         public override byte[] Buffer => BuildBuffer();
+
+        public bool? IsReliable
+        {
+            get
+            {
+                if (Size < 7)
+                    return null;
+
+                return Convert.ToBoolean(ToArray()[6]);
+            }
+        }
+
+        public byte? ChannelId
+        {
+            get
+            {
+                if (Size < 6)
+                    return null;
+
+                return ToArray()[5];
+            }
+        }
+
+        public PhotonCode? PhotonPacketType
+        {
+            get
+            {
+                if (Size < 9)
+                    return null;
+
+                return (PhotonCode)ToArray()[8];
+            }
+        }
+
+        public enum PhotonCode : byte
+        {
+            Init,
+            InitResponse,
+            Operation = 2,
+            OperationResponse,
+            Event,
+            ClientKey = 6,
+            ServerKey
+        }
     }
 }
